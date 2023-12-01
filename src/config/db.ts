8ded150c704eab75ec4c2fa06e7f62dc62f4ebb5
Aravind Sanjeev <saravind436@gmail.com>
2023-12-01 12:28:54 +0530

    changed mongo uri

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGO_URI = "mongodb://127.0.0.1:27017/HackerConnect"
+const MONGO_URI = "mongodb://0.0.0.0:27017/HackerConnect"
 
 
 const connectDB = async () => {
